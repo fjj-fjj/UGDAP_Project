4f7f9b6e8c0b8cfe3e159c0d774c7e4a4c4c8931
fjj-fjj <113220429+fjj-fjj@users.noreply.github.com>
2023-08-13 23:41:43 +0800

    更新了

--- Assets/Scripts/PauseUIController.cs
+++ Assets/Scripts/PauseUIController.cs
@@ -10,7 +10,8 @@ public class PauseUIController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        pauseUI.SetActive(false);
+        settingUI.SetActive(false);
     }
 
     // Update is called once per frame
